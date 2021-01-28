20a49a14a587456463eb2a44e29fe4fbb4f09d11
carlos.dasilva <carlos.dasilva@goias.gov.br>
2021-01-28 17:00:37 -0300

    Terminando configuração do WebConfigSecurity

--- src/main/java/curso/api/spring/rest/security/WebConfigSecurity.java
+++ src/main/java/curso/api/spring/rest/security/WebConfigSecurity.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -40,13 +41,13 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 
         /** Mapeia URL de logout e valida o usuário **/
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 
         /** filtrar requisições de login para autenticação **/
-
+        .and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
 
         /** filtrar demais requisições para verificar a presença do TOKEN JWT no HEADER HTTP **/
+        .addFilterBefore(new JWTApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
     }
